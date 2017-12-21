2b482054db68bfd8cf46ca4e4dd1319db5e6206a
Santiago96 <santicollazosbarrera@hotmail.com>
2017-12-21 15:27:43 -0500

    Modulo Areas
    Se genera el servicio para listar las areas

---
+++ PruebaGrupoASD/src/main/java/com/grupoasd/prueba/modelo/Area.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupoasd.prueba.modelo;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Area {
+    
+    
+    private int idArea;
+    private String nombreArea;
+    private String nombreCiudad;
+
+    public String getNombreCiudad() {
+        return nombreCiudad;
+    }
+
+    public void setNombreCiudad(String nombreCiudad) {
+        this.nombreCiudad = nombreCiudad;
+    }
+
+    public String getNombreArea() {
+        return nombreArea;
+    }
+
+    public void setNombreArea(String nombreArea) {
+        this.nombreArea = nombreArea;
+    }
+
+    public int getIdArea() {
+        return idArea;
+    }
+
+    public void setIdArea(int idArea) {
+        this.idArea = idArea;
+    }
+    
+    
+}
